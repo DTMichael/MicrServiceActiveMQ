c71a4d5e57e6ab13f6aadfb6504c3273f32050da
DTMichael <Zhuyi2632@163.com>
2019-12-05 21:24:08 +0800

    创建项目

---
+++ activemq-eureka/src/test/java/com/atguigu/activemqeureka/ActivemqEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.activemqeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivemqEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
